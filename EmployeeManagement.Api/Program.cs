0fd27895e782aba042719d59cd2a6f666800c3c7
SkyGet0 <239184206+SkyGet0@users.noreply.github.com>
2026-01-11 20:26:54 +0300

    small fixes

--- EmployeeManagement.Api/Program.cs
+++ EmployeeManagement.Api/Program.cs
@@ -1,8 +1,13 @@
+using EmployeeManagement.Api.Services;
+using EmployeeManagement.Core.Repositories;
+using EmployeeManagement.Core.Services;
+using EmployeeManagement.Data;
+using EmployeeManagement.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -11,12 +16,29 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// AutoMapper (позже настроим)
+// AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
+
+// Repositories
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+// Services
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+// CORS (для frontend позже)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-// Configure pipeline
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -24,6 +46,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
 
