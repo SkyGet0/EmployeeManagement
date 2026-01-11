a0828a72ee27524d40e492bf4e43559451642bfd
SkyGet0 <239184206+SkyGet0@users.noreply.github.com>
2026-01-12 02:12:39 +0300

    added new dtos for user login, auth, register

---
+++ EmployeeManagement.Core/DTOs/LoginRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Core.DTOs
+{
+    public class LoginRequestDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(100)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6,
+            ErrorMessage = "Password must be between 6 and 100 characters")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ EmployeeManagement.Core/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Core.DTOs
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
