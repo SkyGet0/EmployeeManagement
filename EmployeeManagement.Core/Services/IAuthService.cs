d938fb746af14f4c0bbafe7ea10600ba4ec2cd04
SkyGet0 <239184206+SkyGet0@users.noreply.github.com>
2026-01-12 02:09:29 +0300

    updated auth interface

---
+++ EmployeeManagement.Core/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeManagement.Core.DTOs;
+
+namespace EmployeeManagement.Core.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync(RegisterRequestDto dto);
+        Task<AuthResponseDto> LoginAsync(LoginRequestDto dto);
+    }
+}
