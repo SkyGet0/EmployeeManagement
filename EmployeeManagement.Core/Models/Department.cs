aa78cb7c8aa8e3dd92751b68e2c96831ac11d2b0
SkyGet0 <239184206+SkyGet0@users.noreply.github.com>
2026-01-12 20:43:38 +0300

    removed usused imports

--- EmployeeManagement.Core/Models/Department.cs
+++ EmployeeManagement.Core/Models/Department.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EmployeeManagement.Core.Models
+﻿namespace EmployeeManagement.Core.Models
 {
     public class Department
     {
